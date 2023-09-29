ad9b2f731ec8bcae01ebb34108c645aa8c8f4be9
Yuji Arima <arimayuji2@gmail.com>
2023-09-29 19:37:50 -0300

    fixing default layout size

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  max-width: 74rem;
-  height: calc(100vh - 10rem);
+  max-width: 50rem;
+  height: fit-content;
   margin: 5rem auto;
-  padding: 2.5rem;
+  padding: 2rem;
   background: ${(props) => props.theme['gray-800']};
   border-radius: 8px;
   display: flex;
