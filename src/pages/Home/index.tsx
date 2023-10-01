93d1ff944c26d3d84960449cad6a866cf0376688
Yuji Arima <arimayuji2@gmail.com>
2023-10-01 20:43:17 -0300

    integrating zod validator !

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import { Play } from 'phosphor-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import {
   CountDownContainer,
   FormContainer,
@@ -9,12 +11,52 @@ import {
   TaskInput,
 } from './styles'
 import { useForm } from 'react-hook-form'
+import { useState } from 'react'
 // controlled / uncontrolled - conceito relacionado a trativa de inputs, sendo controlled é o controle a todo instante e uncontrolled apenas quando houver submit do form
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Its necessary to inform the task!'),
+  minutesAmount: zod
+    .number()
+    .min(5)
+    .max(60, 'The maximum minutes amount is 60 min to a cycle'),
+})
+
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// } - usa interface quando criamos uma tipagem completamente nova
+//  usamos type quando herdamos uma tipagem
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
-  function handleCreateNewCycle(data: any) {
-    console.log(data)
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime())
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+    // sempre que uma alteração de estado depender do valor anterior, utiliza-se uma arrow function para isso
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    reset()
   }
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+  console.log(activeCycle)
   // observando apenas o campo task
   const task = watch('task')
   const isSubmitDisabled = !task
