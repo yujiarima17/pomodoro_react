386ded8bb70aac950c3b38175666dcf07ce95158
Yuji Arima <arimayuji2@gmail.com>
2023-10-07 20:04:38 -0300

    folders structure

---
+++ src/pages/Home/components/Countdown/index.tsx
@@ -0,0 +1,73 @@
+import { useContext, useEffect } from 'react'
+import { CountDownContainer, Separator } from './styles'
+import { differenceInSeconds } from 'date-fns'
+import { CyclesContext } from '../..'
+
+export function Countdown() {
+  const {
+    activeCycle,
+    activeCycleId,
+    markCurrentCycleAsFinished,
+    amountSecondsPassed,
+    setSecondsPassed,
+  } = useContext(CyclesContext)
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+
+  useEffect(() => {
+    let interval: number
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate,
+        )
+        if (secondsDifference >= totalSeconds) {
+          markCurrentCycleAsFinished()
+          setSecondsPassed(secondsDifference)
+          clearInterval(interval)
+        } else {
+          setSecondsPassed(secondsDifference)
+        }
+      }, 1000)
+    }
+
+    /* é necessário utilizar essa clean function porque quando
+       settamos uma novo ciclo, a setInterval não irá parar pois o activeCycle continua true
+       e a diferença de troca de ciclo não pare a função setInterval, e devido a isso o clock
+       nao começa no tempo certo. E ele começa a descontar segundos a partir do ultimo intervalo
+       do ciclo anteriro
+     */
+    return () => {
+      clearInterval(interval)
+    }
+  }, [
+    activeCycle,
+    totalSeconds,
+    activeCycleId,
+    markCurrentCycleAsFinished,
+    setSecondsPassed,
+  ])
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+  // forma de mostrar o zero inicial quando o tempo torna-se menor que 10, ou seja, 09/ 08
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) document.title = `${minutes}:${seconds}`
+  }, [minutes, seconds, activeCycle])
+
+  return (
+    <CountDownContainer>
+      <span>{minutes[0]}</span>
+      <span>{minutes[1]}</span>
+
+      <Separator>:</Separator>
+
+      <span>{seconds[0]}</span>
+      <span>{seconds[1]}</span>
+    </CountDownContainer>
+  )
+}
---
+++ src/pages/Home/components/NewCyclesForm/index.tsx
@@ -0,0 +1,39 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from '../../styles'
+import { useFormContext } from 'react-hook-form'
+import { CyclesContext } from '../..'
+import { useContext } from 'react'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+  return (
+    <FormContainer>
+      <label htmlFor="task">I will work in :</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        {...register('task')}
+      />
+      <datalist id="task-suggestions">
+        <option value="Projeto 1"></option>
+        <option value="Projeto 2"></option>
+        <option value="Projeto 3"></option>
+        <option value="Projeto 4"></option>
+      </datalist>
+      <label htmlFor="">period</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        disabled={!!activeCycle}
+        max={60}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutes.</span>
+    </FormContainer>
+  )
+}
